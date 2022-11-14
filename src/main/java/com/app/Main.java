08665307880196d8e3b18535a996cf2fd4ac18ac
T-H9703EnAc <t.h.1997.m.engineer.ac@gmail.com>
2022-11-15 00:55:21 +0900

    DB接続処理追加

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,12 +1,47 @@
 package com.app;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		String url = "jdbc:postgresql://localhost:5432/test";
+		String user = "testuser";
+		String password = "testuser";
+		String sql = "select * from test_schema.user_infomation;";
 		
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO 自動生成された catch ブロック
+			e1.printStackTrace();
+		}
+
+		try (Connection conn = DriverManager.getConnection(url, user, password);
+				PreparedStatement ps = conn.prepareStatement(sql)) {
+			
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					System.out.println(
+							rs.getInt("id") + " " +
+									rs.getString("name") + " " +
+									rs.getString("gender") +
+									rs.getString("hometown"));
+				}
+			}
+			;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("処理が完了しました");
+		}
+
 	}
 
 }
